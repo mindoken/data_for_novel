6739851d3fa605f665cf2d523db09b63780fc4e2
Vladislav Melnikov <melnikoff2222@gmail.com>
2024-12-22 23:21:30 +0300

    yes

---
+++ data_for_novel/imageImports.tsx
@@ -0,0 +1,71 @@
+import image1 from './images/5307596327154213633.jpg';
+import image2 from './images/5307596327154213633.jpg';
+import image3 from './images/5307596327154213633.jpg';
+import image4 from './images/5307596327154213633.jpg';
+import image5 from './images/5307596327154213633.jpg';
+import image6 from './images/5307596327154213633.jpg';
+import image7 from './images/5307596327154213633.jpg';
+import image8 from './images/5307596327154213633.jpg';
+import image9 from './images/5307596327154213633.jpg';
+import image10 from './images/5307596327154213633.jpg';
+import image11 from './images/5307596327154213633.jpg';
+import image12 from './images/5307596327154213633.jpg';
+import image13 from './images/5307596327154213633.jpg';
+import image14 from './images/5307596327154213633.jpg';
+import image15 from './images/5307596327154213633.jpg';
+import image16 from './images/5307596327154213633.jpg';
+import image17 from './images/5307596327154213633.jpg';
+import image18 from './images/5307596327154213633.jpg';
+import image19 from './images/5307596327154213633.jpg';
+import image20 from './images/5307596327154213633.jpg';
+import image21 from './images/5307596327154213633.jpg';
+import image22 from './images/5307596327154213633.jpg';
+import image23 from './images/5307596327154213633.jpg';
+import image24 from './images/5307596327154213633.jpg';
+import image25 from './images/5307596327154213633.jpg';
+import image26 from './images/5307596327154213633.jpg';
+import image27 from './images/5307596327154213633.jpg';
+import image28 from './images/5307596327154213633.jpg';
+import image29 from './images/5307596327154213633.jpg';
+import image30 from './images/5307596327154213633.jpg';
+import image31 from './images/5307596327154213633.jpg';
+import image32 from './images/5307596327154213633.jpg';
+import image33 from './images/5307596327154213633.jpg';
+const images = {
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+    image11,
+    image12,
+    image13,
+    image14,
+    image15,
+    image16,
+    image17,
+    image18,
+    image19,
+    image20,
+    image21,
+    image22,
+    image23,
+    image24,
+    image25,
+    image26,
+    image27,
+    image28,
+    image29,
+    image30,
+    image31,
+    image32,
+    image33,
+    // Добавьте остальные изображения здесь
+  } as const;
+  export type ImageKeys = keyof typeof images;
+  export default images;
